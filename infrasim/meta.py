09f93e1392bd74281d6cdcd9e1983b3a3d495a89
Aman Majid <39566844+amanmajid@users.noreply.github.com>
2021-08-10 10:41:02 +0100

    Forked infrasim into local dir

---
+++ infrasim/meta.py
@@ -0,0 +1,22 @@
+'''
+    metainfo.py
+        Meta-data associated with the INFRASIM model
+
+    @amanmajid
+'''
+
+metainfo = {
+            'i_field'               : 'from_id',
+            'j_field'               : 'to_id',
+            'cost_column'           : 'length_km',
+            'upper_bound'           : 'max',
+            'lower_bound'           : 'min',
+            'nodes_header'          : ['id','asset_type','subtype','title','capacity'],
+            'edges_header'          : ['id','from_id','to_id','length_km'],
+            'flow_header'           : ['timestep'],
+            'edge_index_variables'  : ['from_id','to_id','timestep'],
+            'infrasim_cache'        : '../data/__infrasim__/',
+            'outputs_figures'       : '../outputs/figures/',
+            'outputs_data'          : '../outputs/statistics/',
+            'temporal_resolution'   : 'daily'
+            }
