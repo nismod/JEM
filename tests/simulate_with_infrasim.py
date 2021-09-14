f9be2a9cf3b4551910a1bc0190b6e4a70379effa
Aman Majid <39566844+amanmajid@users.noreply.github.com>
2021-09-14 12:43:12 +0100

    <wip> debugging scale up

--- tests/simulate_with_infrasim.py
+++ tests/simulate_with_infrasim.py
@@ -12,6 +12,7 @@ import sys
 sys.path.append('../')
 
 from infrasim.model import *
+from infrasim.utils import *
 
 #------------------------------------
 # RUN MODEL
@@ -53,25 +54,29 @@ jem.run(print_to_console=True)
 # PROCESS RESULTS OR DEBUG
 
 try:
-    
-    r = jem.results_arcflows
-    r = r[r.to_id != 'super_sink'].reset_index(drop=True)
-    
+        
     #---
     # Export results to GIS
     
     edges = gpd.read_file(path_to_edges)
     nodes = gpd.read_file(path_to_nodes)
     
-    # concat
-    rr = pd.concat([edges,r],axis=1).sort_values(by='id')
-    edges = edges.sort_values(by='id')
+    # tag nodes that were supplied by super_source
+    jem = tag_super_source_flows(jem)
     
-    # add to edges
-    edges['Result'] = rr.Value
     
     # save
-    edges.to_file(driver='ESRI Shapefile', filename='../data/demo/model_results.shp')
+    jem.nodes.to_file(driver='ESRI Shapefile', filename='../data/demo/jem_results_nodes.shp')
+    
+    # # concat
+    # rr = pd.concat([edges,r],axis=1).sort_values(by='id')
+    # edges = edges.sort_values(by='id')
+    
+    # # add to edges
+    # edges['Result'] = rr.Value
+    
+    # # save
+    # edges.to_file(driver='ESRI Shapefile', filename='../data/demo/model_results.shp')
     
 except:
     jem.debug()
