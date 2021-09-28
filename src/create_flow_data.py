7023fe86218564f7702398853d3076dafd49c505
Aman Majid <39566844+amanmajid@users.noreply.github.com>
2021-09-28 14:06:07 +0100

    Completed load profiles

--- src/create_flow_data.py
+++ src/create_flow_data.py
@@ -18,37 +18,30 @@ sys.path.append("../../")
 
 # Import local functions
 from utils import *
-
+from merge_elec_consumption_data import *
 
 #=======================
 # PROCESSING
 
-def compute_demand(nodes):
+def compute_supply_demand(nodes):
     '''Calculate demand based on population and ei
     '''
+    nodes['flow'] = 0
+    # supply
+    nodes.loc[nodes.asset_type == 'source', 'flow'] = \
+        nodes.loc[nodes.asset_type == 'source', 'capacity']
+    # demand
+    nodes.loc[nodes.asset_type == 'sink', 'flow'] = \
+        nodes.loc[nodes.asset_type == 'sink', 'population'] * \
+            nodes.loc[nodes.asset_type == 'sink', 'ei']
+    return nodes
     
 
 # read data
 network = read_data(path_to_nodes='../data/spatial/nodes_processed.shp')
+print('loaded data')
 
 # flow nodes
 flow_nodes = get_flow_nodes(network).reset_index(drop=True)
-
-flow_nodes['flow'] = 0
-
-flow_nodes.loc[flow_nodes.asset_type == 'source', 'flow'] = \
-    flow_nodes.loc[flow_nodes.asset_type == 'source', 'capacity'] * 1000
-
-flow_nodes.loc[flow_nodes.asset_type == 'sink', 'flow'] = \
-    flow_nodes.loc[flow_nodes.asset_type == 'sink', 'population'] * \
-        flow_nodes.loc[flow_nodes.asset_type == 'sink', 'ei']
-
-
-
-# # supplies
-# supply_nodes = network.nodes.loc[network.nodes.asset_type == 'source'].reset_index(drop=True)
-
-# supply_nodes = supply_nodes[['id','capacity']]
-
-# # pivot
-# t = supply_nodes.pivot_table(columns='id')
+flow_nodes = compute_supply_demand(flow_nodes)
+flow_nodes['flow'] = flow_nodes['flow'] * 1000  # convert to kW
