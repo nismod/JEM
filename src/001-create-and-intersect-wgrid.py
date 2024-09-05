2f3a86ceb7bc8f7d426f80dea52fcd9e72202524
Tom Russell <tom.russell@ouce.ox.ac.uk>
2024-09-05 11:45:44 +0100

    Rewrite grid setup and intersection script
    - break into functions
    - drop commented-out sections
    - add command-line parameter for data directory

--- src/001-create-and-intersect-wgrid.py
+++ src/001-create-and-intersect-wgrid.py
@@ -1,38 +1,68 @@
 #!/usr/bin/env python
 # coding: utf-8
+"""Set up hotspots grid, then split power network lines and nodes
+
+1. create an empty 1km grid for Jamaica. Create an empty TIFF file for a given
+   box specifying the spatial grid to evaluate network failure, create integer
+   grid IDs for each cell.
+2. assign grid IDs to power network nodes and (split) edges
 
 
-# home directory on cluster: /soge-home/projects/mistral/jamaica-ccri
-# data on cluster is in:
-# ./processed_data/boundaries/admin_boundaries.gpkg --> /soge-home/projects/mistral/jamaica-ccri/boundaries/jamaica.gpkg
-# ./processed_data/networks/energy/electricity_network_v3.1.gpkg --> /soge-home/projects/mistral/jamaica-ccri/processed_data/networks/energy/electricity_network_v3.2.gpkg
+See also, for grid creation:
+https://github.com/nismod/open-gira/blob/main/workflow/tropical-cyclone/wind_fields/wind_fields.smk#L84
 
+Data
+----
 
-import json
+Inputs:
+
+- ./processed_data/boundaries/jamaica.gpkg
+- ./processed_data/networks/energy/electricity_network_v3.2.gpkg
+
+Outputs:
+
+- ./results/grid_failures/jamaica_1km_empty_grid.tiff
+- ./results/grid_failures/jamaica_1km_grid_ids.tiff
+- ./results/grid_failures/jamaica_electricity_network_wgrid_ids.gpkg
+
+Usage
+-----
+
+To run on SoGE cluster:
+
+    python 001-create-and-intersect-wgrid.py /soge-home/projects/mistral/jamaica-ccri
+
+To run locally with a relative path, ensure all inputs are available, then:
+
+    python 001-create-and-intersect-wgrid.py ./data/dir
+
+"""
 import os
 import sys
+from pathlib import Path
 
 import geopandas as gpd
 import numpy as np
 import pandas as pd
 import rasterio
-import shapely
 import snail
-import snail.intersection
-import snail.io
 
-from pathlib import Path
-from glob import glob
 
-# create an empty 1km grid for Jamaica
+def main():
+    # base_path = Path("/soge-home/projects/mistral/jamaica-ccri/")
+    base_path = Path("./data/ccri")
 
-# ref: https://github.com/nismod/open-gira/blob/main/workflow/tropical-cyclone/wind_fields/wind_fields.smk#L84
-"""
-Create an empty TIFF file for a given box specifying the spatial grid to
-evaluate network failure
-"""
+    # input paths
+    boundary_file_path = base_path / "processed_data/boundaries/jamaica.gpkg"
 
-output_path = "/soge-home/projects/mistral/jamaica-ccri/results/grid_failures/jamaica_1km_empty_grid.tiff"
+    # output paths
+    empty_grid_path = base_path / "results/grid_failures/jamaica_1km_empty_grid.tiff"
+    grid_id_tiff_path = base_path / "results/grid_failures/jamaica_1km_grid_ids.tiff"
+
+    create_grid(grid_id_tiff_path, boundary_file_path, empty_grid_path)
+    grid, hazard_files = read_grid(grid_id_tiff_path)
+    intersect_grid_with_nodes(base_path, grid, hazard_files)
+    intersect_grid_with_edges(base_path, grid, hazard_files)
 
 
 def harmonise_grid(
@@ -57,189 +87,110 @@ def harmonise_grid(
     return n_cells, minimum - buffer, maximum + buffer
 
 
-filename = "/soge-home/projects/mistral/jamaica-ccri/processed_data/boundaries/jamaica.gpkg"
-bounds = gpd.read_file(filename, layer="jamaica")
-print(bounds.crs)  # EPSG:3448
+def create_grid(grid_id_tiff_path, boundary_file_path, empty_grid_path):
+    bounds = gpd.read_file(boundary_file_path, layer="jamaica")
 
-# expand grid by buffer
-buffer = 100  # in meters (based on crs of bounds)
-boundary_box = bounds.bounds
-minx, miny, maxx, maxy = boundary_box.values[0]
-minx -= buffer
-miny -= buffer
-maxx += buffer
-maxy += buffer
-# cell side length in meters (based on crs of bounds)
-cell_length = 1000
-# determine grid bounding box to fit an integer number of grid cells in each dimension
-i, minx, maxx = harmonise_grid(minx, maxx, cell_length)
-j, miny, maxy = harmonise_grid(miny, maxy, cell_length)
-print(i, j, minx, maxx, miny, maxy)
-# create grid as TIFF and save to disk
-os.makedirs(os.path.dirname(output_path), exist_ok=True)
-command = f"gdal_create -outsize {i} {j} -a_srs EPSG:3448 -a_ullr {minx} {miny} {maxx} {maxy} {output_path}"  # note: projection is hard-coded
-os.system(command)
+    # expand grid by buffer
+    buffer = 100  # in meters (based on crs of bounds)
+    boundary_box = bounds.bounds
+    minx, miny, maxx, maxy = boundary_box.values[0]
+    minx -= buffer
+    miny -= buffer
+    maxx += buffer
+    maxy += buffer
 
-# create grid ids, intersect empty grid with points, get corresponding grid ids for point intersections
+    # cell side length in meters (based on crs of bounds)
+    cell_length = 1000
 
-# Load the .tiff file
-tiff_file_path = "/soge-home/projects/mistral/jamaica-ccri/results/grid_failures/jamaica_1km_empty_grid.tiff"
-with rasterio.open(tiff_file_path) as src:
-    # Extract grid data
-    grid_ids = np.arange(src.width * src.height).reshape((src.height, src.width))
-    grid_transform = src.transform
-    grid_width = src.width
-    grid_height = src.height
+    # determine grid bounding box to fit an integer number of grid cells in each dimension
+    i, minx, maxx = harmonise_grid(minx, maxx, cell_length)
+    j, miny, maxy = harmonise_grid(miny, maxy, cell_length)
 
+    # create grid as TIFF and save to disk
+    os.makedirs(os.path.dirname(empty_grid_path), exist_ok=True)
+    command = f"gdal_create -outsize {i} {j} -a_srs EPSG:3448 -a_ullr {minx} {miny} {maxx} {maxy} {empty_grid_path}"  # note: projection is hard-coded
+    os.system(command)
 
-# Export grid_ids as .tiff file
-tiff_file_path = "/soge-home/projects/mistral/jamaica-ccri/results/grid_failures/jamaica_1km_grid_ids.tiff"
+    # Load the .tiff file
+    with rasterio.open(empty_grid_path) as src:
+        # Extract grid data
+        grid_ids = np.arange(src.width * src.height).reshape((src.height, src.width))
+        grid_transform = src.transform
+        grid_width = src.width
+        grid_height = src.height
 
-with rasterio.open(tiff_file_path, 'w', driver='GTiff', height=grid_height, width=grid_width,
-                   count=1, dtype=rasterio.uint32, crs=src.crs, transform=grid_transform) as dst:
-    dst.write(grid_ids.astype(rasterio.uint32), 1)
-
-
-# TODO: update sections below to get value of raster file instead of querying grid_ids (although still a valid approach)
-
-# Read the points dataset
-points_df = gpd.read_file(
-    "/soge-home/projects/mistral/jamaica-ccri/processed_data/networks/energy/electricity_network_v3.2.gpkg",
-    layer="nodes",
-)
-
-##### v01 #####
-# # print(points_df.crs)
-# points_df["x"] = points_df["geometry"].x
-# points_df["y"] = points_df["geometry"].y
-
-# # Extract coordinates into numpy arrays
-# x_coords = points_df["x"].to_numpy()
-# y_coords = points_df["y"].to_numpy()
-
-# # Calculate the grid cell indices for each point
-# cols = ((x_coords - grid_transform.c) / grid_transform.a).astype(int)
-# #rows = ((y_coords - grid_transform.f) / -grid_transform.e).astype(int)
-# rows = ((grid_transform.f - y_coords) / grid_transform.e).astype(int)
-
-# # Add grid cell information to the dataframe
-# points_df["grid_row"] = rows
-# points_df["grid_col"] = cols
-
-# # grid_ids = grid_ids.reshape((grid_height, grid_width))
-# point_grid_ids = grid_ids[rows, cols]
-# points_df["grid_ids"] = point_grid_ids
-
-##### v02 #####
-hazard_paths = [
-    "/soge-home/projects/mistral/jamaica-ccri/results/grid_failures/jamaica_1km_grid_ids.tiff"
-]
-hazard_files = pd.DataFrame({"path": hazard_paths})
-hazard_files['key'] = [Path(path).stem for path in hazard_paths]
-hazard_files, grids = snail.io.extend_rasters_metadata(hazard_files)
-grid = grids[0]
-grid_intersections_p = snail.intersection.apply_indices(
-    points_df, grid, index_i="i_0", index_j="j_0"
-)
-
-grid_intersections_p = snail.io.associate_raster_files(
-    grid_intersections_p, hazard_files
-)
-
-grid_intersections_p.rename(columns={"jamaica_1km_grid_ids":"grid_ids"}, inplace =True)
-
-grid_intersections_p.to_file(
-    "/soge-home/projects/mistral/jamaica-ccri/results/grid_failures/jamaica_electricity_nodes_wgrid_ids.gpkg",
-    layer="nodes",
-    driver="gpkg",
-)
-
-# intersect grid with edges using snail library to calculate length of interecting segments
-# for the split linestrings get centroids
-# get corresponding grid ids for centroids
-# merge linestings back, replacing temporary centroids geometry
-
-hazard_paths = [
-    "/soge-home/projects/mistral/jamaica-ccri/results/grid_failures/jamaica_1km_grid_ids.tiff"
-]
-hazard_files = pd.DataFrame({"path": hazard_paths})
-hazard_files['key'] = [Path(path).stem for path in hazard_paths]
-hazard_files, grids = snail.io.extend_rasters_metadata(hazard_files)
-grid = grids[0]
-edges = gpd.read_file(
-    "/soge-home/projects/mistral/jamaica-ccri/processed_data/networks/energy/electricity_network_v3.2.gpkg",
-    layer="edges",
-)
-edges = snail.intersection.prepare_linestrings(edges)
-grid_intersections_l = snail.intersection.split_linestrings(edges, grid)
-print(edges.crs)  # epsg:3448 is in meters
-grid_intersections_l["length_m"] = grid_intersections_l["geometry"].length
-grid_intersections_l = snail.intersection.apply_indices(
-    grid_intersections_l, grid, index_i="i_0", index_j="j_0"
-)
-
-grid_intersections_l = snail.io.associate_raster_files(
-    grid_intersections_l, hazard_files
-)
-
-grid_intersections_l.rename(columns={"jamaica_1km_grid_ids":"grid_ids"}, inplace =True)
-
-grid_intersections_l.to_file(
-    "/soge-home/projects/mistral/jamaica-ccri/results/grid_failures/jamaica_electricity_nodes_wgrid_ids.gpkg",
-    layer="edges",
-    driver="gpkg",
-)
-
-# points_df = gpd.read_file(
-#     "/soge-home/projects/mistral/jamaica-ccri/results/grid_failures/jamaica_electricity_nodes_wgrid_ids.gpkg",
-#     layer="nodes",
-#     # driver="gpkg",
-# )
-# lines_df = grid_intersections.copy()
-# lines_df["geometry"] = lines_df["geometry"].centroid
-
-# # Load the .tiff file
-# tiff_file_path = "/soge-home/projects/mistral/jamaica-ccri/results/grid_failures/jamaica_1km_empty_grid.tiff"
-# with rasterio.open(tiff_file_path) as src:
-#     # Extract grid data
-#     grid_ids = np.arange(src.width * src.height).reshape((src.height, src.width))
-#     grid_transform = src.transform
-#     grid_width = src.width
-#     grid_height = src.height
-
-# # print(gdf.crs)
-# lines_df["x"] = lines_df["geometry"].x
-# lines_df["y"] = lines_df["geometry"].y
-
-# # Extract coordinates into numpy arrays
-# x_coords = lines_df["x"].to_numpy()
-# y_coords = lines_df["y"].to_numpy()
-
-# # Calculate the grid cell indices for each point
-# cols = ((x_coords - grid_transform.c) / grid_transform.a).astype(int)
-# #rows = ((y_coords - grid_transform.f) / -grid_transform.e).astype(int)
-# rows = ((grid_transform.f - y_coords) / grid_transform.e).astype(int)
-
-# # Add grid cell information to the dataframe
-# lines_df["grid_row"] = rows
-# lines_df["grid_col"] = cols
+    # Export grid_ids as .tiff file
+    with rasterio.open(
+        grid_id_tiff_path,
+        "w",
+        driver="GTiff",
+        height=grid_height,
+        width=grid_width,
+        count=1,
+        dtype=rasterio.uint32,
+        crs=src.crs,
+        transform=grid_transform,
+    ) as dst:
+        dst.write(grid_ids.astype(rasterio.uint32), 1)
 
 
-# # grid_ids = grid_ids.reshape((grid_height, grid_width))
-# point_grid_ids = grid_ids[rows, cols]
-# lines_df["grid_ids"] = point_grid_ids
-# del lines_df["geometry"]
-# lines_geo_ = edges[["id", "geometry"]]
-# lines_df = pd.merge(lines_geo_, lines_df, how="outer", left_on="id", right_on="id")
-
-# lines_df.to_file(
-#     "/soge-home/projects/mistral/jamaica-ccri/results/grid_failures/jamaica_electricity_nodes_wgrid_ids.gpkg",
-#     layer="edges",
-#     driver="gpkg",
-# )
+def read_grid(grid_id_tiff_path):
+    hazard_paths = [grid_id_tiff_path]
+    hazard_files = pd.DataFrame({"path": hazard_paths})
+    hazard_files["key"] = [Path(path).stem for path in hazard_paths]
+    hazard_files, grids = snail.io.extend_rasters_metadata(hazard_files)
+    grid = grids[0]
+    return grid, hazard_files
 
 
-# import matplotlib.pyplot as plt
-# fig, ax = plt.subplots()
-# points_df[points_df['grid_ids'] == 14969].plot(ax = ax, markersize = 0.3, color = 'red')
-# lines_df[lines_df['grid_ids'] == 14969].plot(ax = ax, color = 'blue', alpha = 0.3)
+def intersect_grid_with_nodes(base_path, grid, hazard_files):
+    points_df = gpd.read_file(
+        base_path / "processed_data/networks/energy/electricity_network_v3.2.gpkg",
+        layer="nodes",
+    )
+    grid_intersections_p = snail.intersection.apply_indices(
+        points_df, grid, index_i="i_0", index_j="j_0"
+    )
+    grid_intersections_p = snail.io.associate_raster_files(
+        grid_intersections_p, hazard_files
+    )
+    grid_intersections_p.rename(
+        columns={"jamaica_1km_grid_ids": "grid_ids"}, inplace=True
+    )
+    grid_intersections_p.to_file(
+        base_path / "results/grid_failures/jamaica_electricity_network_wgrid_ids.gpkg",
+        layer="nodes",
+        driver="gpkg",
+    )
+
+
+def intersect_grid_with_edges(base_path, grid, hazard_files):
+    edges = gpd.read_file(
+        base_path / "processed_data/networks/energy/electricity_network_v3.2.gpkg",
+        layer="edges",
+    )
+    edges = snail.intersection.prepare_linestrings(edges)
+    grid_intersections_l = snail.intersection.split_linestrings(edges, grid)
+    grid_intersections_l["length_m"] = grid_intersections_l["geometry"].length
+    grid_intersections_l = snail.intersection.apply_indices(
+        grid_intersections_l, grid, index_i="i_0", index_j="j_0"
+    )
+    grid_intersections_l = snail.io.associate_raster_files(
+        grid_intersections_l, hazard_files
+    )
+    grid_intersections_l.rename(
+        columns={"jamaica_1km_grid_ids": "grid_ids"}, inplace=True
+    )
+    grid_intersections_l.to_file(
+        base_path / "results/grid_failures/jamaica_electricity_network_wgrid_ids.gpkg",
+        layer="edges",
+        driver="gpkg",
+    )
+
+
+if __name__ == "__main__":
+    try:
+        base_path = sys.argv[1]
+    except IndexError:
+        print(f"Usage: python {__file__} ./path/to/data/directory")
+    main(base_path)
