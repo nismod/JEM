069c8ddb9feea24ed6997523d3c54aa35a3072e7
Tom Russell <tom.russell@ouce.ox.ac.uk>
2024-09-05 11:43:13 +0100

    Move jem to src/jem

---
+++ src/jem/meta.py
@@ -0,0 +1,22 @@
+"""
+    metainfo.py
+        Meta-data associated with the INFRASIM model
+
+    @amanmajid
+"""
+
+metainfo = {
+    "i_field": "from_id",
+    "j_field": "to_id",
+    "cost_column": "length",
+    "upper_bound": "max",
+    "lower_bound": "min",
+    "nodes_header": ["id", "asset_type", "subtype", "title", "capacity"],
+    "edges_header": ["id", "from_id", "to_id", "length", "min", "max"],
+    "flow_header": [],
+    "edge_index_variables": ["from_id", "to_id", "timestep"],
+    "infrasim_cache": "../data/__infrasim__/",
+    "outputs_figures": "../outputs/figures/",
+    "outputs_data": "../outputs/statistics/",
+    "temporal_resolution": "daily",
+}
---
+++ src/jem/params.py
@@ -0,0 +1,14 @@
+"""
+    constants.py
+        Assumed constants in the INFRASIM model
+
+    @amanmajid
+"""
+
+variables = {}
+
+
+constants = {
+    "super_source_maximum": 10**12,
+    "mask_value": -999,
+}
